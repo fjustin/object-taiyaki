3e3b881b8318e356e8903e93e85a4ea1e205c789
futamidaiki <nimi0370@gmail.com>
2017-11-26 00:16:33 +0900

    Can check expired or not

--- taiyaki_test.rb
+++ taiyaki_test.rb
@@ -1,7 +1,8 @@
 require 'minitest/autorun'
+require 'date'
 
 class Taiyaki
-  attr_reader :anko,:size
+  attr_reader :anko,:size,:produced_on
 
   # 初期状態でたい焼きにあんこを持たせる
   # 元の値段：100円
@@ -10,6 +11,7 @@ class Taiyaki
   def initialize(anko, size)
     @anko = anko
     @size = size
+    @produced_on = Date.today
   end
 
   def price
@@ -23,6 +25,14 @@ class Taiyaki
     end
 
     amount
+  end
+
+  def expire_on
+    produced_on + 3
+  end
+
+  def expired?(today = Date.today)
+    expire_on < today
   end
 
   def to_s
@@ -52,6 +62,24 @@ class TaiyakiTest < MiniTest::Test
     assert_equal '大きめ', taiyaki_3.size
     assert_equal 150,taiyaki_3.price
     assert_equal 'あんこ：あずき,大きさ：大きめ,値段：150円',taiyaki_3.to_s
+
+  end
+
+  def test_expired
+    taiyaki = Taiyaki.new('あずき','ふつう')
+
+    # 製造日 = システム日付
+    assert_equal Date.today, taiyaki.produced_on
+
+    # 賞味期限 = 製造日 + 3日
+    assert_equal (Date.today + 3) ,taiyaki.expire_on
+
+    # 3日目までfalse
+    # 3日待つわけには行かない
+    refute taiyaki.expired?(Date.today + 3)
+
+    # 4日目までtrue
+    assert taiyaki.expired?(Date.today + 4)
 
   end
 end
